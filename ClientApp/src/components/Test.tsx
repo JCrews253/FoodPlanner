a4dc74706ea8d883e8b5a75697d6ce5fca119ed1
James Crews <jcrews206@gmail.com>
2021-08-25 21:55:22 -0700

    5 -> 3.1

--- ClientApp/src/components/Test.tsx
+++ ClientApp/src/components/Test.tsx
@@ -1,22 +1,24 @@
-import gql from "graphql-tag"
-
+import gql from "graphql-tag";
+import { useUsersQuery } from "../gql";
+import React from "react";
 
 gql`
-  query Users($id: ID!){
-    user(id: $id){
-      ... on User{
+  query Users($id: ID!) {
+    user(id: $id) {
+      ... on User {
         id
       }
     }
   }
-`
-
+`;
 
 const Test = () => {
+  const users = useUsersQuery({
+    variables: {
+      id: "",
+    },
+  });
+  return <h1>test component</h1>;
+};
 
-  return (
-    <h1>test component</h1>
-  )
-}
-
-export default Test
+export default Test;
